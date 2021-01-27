34b6c0c021ecda56ae98cd3164031bb83bc4b004
mrxflight <marlon.lange@marienschule-limburg.de>
2021-01-27 12:06:52 +0100

    exclude Header

---
+++ AktuelleProjekte.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stolidus7</title>
+</head>
+<body>
+<?php include ("menuBar.php") ?>
+<section class="section py-6 mt-6">
+    <div class="container">
+        <h1 class="title has-text-centered is-size-1">Minecraft</h1>
+        <h3 class="title has-text-left">Slightly Vanilla</h3>
+    </div>
+</section>
+</body>
+</html>
---
+++ menuBar.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Stolidus7</title>
+</head>
+<body>
+<nav class="navbar is-fixed-top is-dark">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+              <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+            </a>
+        
+            <span class="navbar-burger burger is-dark" data-target="NavMenu">
+              <span class="is-dark"></span>
+              <span class="is-dark"></span>
+              <span class="is-dark"></span>
+            </span>
+          </div>
+        <div class="navbar-menu" id="NavMenu">
+            <div class="navbar-end">
+            <a class="navbar-item is-dark">
+                    Startseite
+                </a>
+                
+                <a class="navbar-item is-dark" href="AktuelleProjekte.php">
+                    Aktuelle Projekte
+                </a>
+
+                <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link is-dark">
+                    Mitglieder
+                </a>                                  
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                        ImperatorArtyvex
+                    </a>
+                    <a class="navbar-item">
+                        SickVoid
+                    </a>
+                    <a class="navbar-item">
+                        Mr. X-flight
+                    </a>
+                </div>
+                </div>
+                <a class="navbar-item is-dark">
+                    DatenÜbertragung
+                </a>
+            </div>
+        </div>
+    </div>
+</nav>
+</body>
